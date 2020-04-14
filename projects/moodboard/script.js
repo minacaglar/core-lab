136efcae66d048e7b8cc4c2f1ab06ccc6173812e
minacaglar <60526174+minacaglar@users.noreply.github.com>
2020-04-14 18:55:44 +0200

    git status

---
+++ projects/moodboard/script.js
@@ -0,0 +1,40 @@
+$(function(){
+
+	// dragging elements
+	$(".draggable").draggable({
+		stack: ".draggable", 
+		containment: ".container"
+	})
+
+	// toggle switch
+	$(".switch").click( function() {
+		$(".container").toggleClass("darkmode");
+	})
+
+	// photo appears on hover
+	$(".hoverme").mouseover( function() {
+		$(".horsegirl").css("display", "block");
+	})
+
+	// photo dissapears on mouse out
+	$(".hoverme").mouseout( function() {
+		$(".horsegirl").css("display", "none");
+	})
+
+	// display hidden text
+	$(".clickme").click( function() {
+		$(".extra-info").fadeToggle("hide");
+	})
+
+	// animate circle
+	$(".circle").click( function() {
+		$(".circle").animate({
+			opacity: 0.5,
+			width: "500px",
+			height: "500px",
+			backgroundColor: "red"
+
+		})
+	})
+
+})
